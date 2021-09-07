ca65c2970ead2ffe807c8e4cc85253fa08b4bbaa
Dyego Soriano <dyegodf2@gmail.com>
2021-09-07 19:24:08 -0300

    feat: adicionando tipagem

--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -1,18 +1,20 @@
 import { Router } from 'express'
-import { v4 as uuid } from 'uuid'
+
+import { Category } from '../model/Category'
 
 const categoriesRoutes = Router()
 
-const categories = []
+const categories: Category[] = []
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body
 
-  const category = { id: uuid(), description, name }
+  const category = new Category()
+  Object.assign(category, { created_at: new Date(), description, name })
 
   categories.push(category)
 
-  return response.status(201).send()
+  return response.status(201).json(category)
 })
 
 export { categoriesRoutes }
