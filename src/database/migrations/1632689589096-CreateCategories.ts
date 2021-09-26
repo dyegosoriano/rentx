086f78dfc00810e96b942f09869f9203d93310d5
Dyego Soriano <dyegodf2@gmail.com>
2021-09-26 18:04:10 -0300

    feat: adding migration

---
+++ src/database/migrations/1632689589096-CreateCategories.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+class CreateCategories1632689589096 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'description',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories')
+  }
+}
+
+export { CreateCategories1632689589096 }
