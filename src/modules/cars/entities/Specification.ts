77b3f1cb127315c6f716824e1e2cbb6e015f1f60
Dyego Soriano <dyegodf2@gmail.com>
2021-09-28 15:03:57 -0300

    refactor: SpecificationRepository

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,10 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 import { v4 as uuid } from 'uuid'
 
+@Entity('specifications')
 class Specification {
-  description: string
-  created_at: Date
-  name: string
+  @PrimaryColumn()
   id?: string
+
+  @Column()
+  description: string
+
+  @Column()
+  name: string
+
+  @CreateDateColumn()
+  created_at: Date
 
   constructor() {
     if (!this.id) this.id = uuid()
--- src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,32 +1,23 @@
+import { getRepository, Repository } from 'typeorm'
+
 import { Specification } from '../../entities/Specification'
 import { ICreateSpecificationDTO, ISpecificationRepository } from '../ISpecificationRepository'
 
 class SpecificationRepository implements ISpecificationRepository {
-  private specifications: Specification[]
+  private repository: Repository<Specification>
 
-  private static INSTANCE: SpecificationRepository
-
-  private constructor() {
-    this.specifications = []
+  constructor() {
+    this.repository = getRepository(Specification)
   }
 
-  public static getInstance(): SpecificationRepository {
-    if (!SpecificationRepository.INSTANCE) {
-      SpecificationRepository.INSTANCE = new SpecificationRepository()
-    }
+  async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({ created_at: new Date(), description, name })
 
-    return SpecificationRepository.INSTANCE
+    await this.repository.save(specification)
   }
 
-  create({ description, name }: ICreateSpecificationDTO): void {
-    const specification = new Specification()
-    Object.assign(specification, { created_at: new Date(), description, name })
-    this.specifications.push(specification)
-  }
-
-  findByName(name: string): Specification {
-    const specification = this.specifications.find(item => item.name === name)
-    return specification
+  async findByName(name: string): Promise<Specification> {
+    return await this.repository.findOne({ name })
   }
 }
 
