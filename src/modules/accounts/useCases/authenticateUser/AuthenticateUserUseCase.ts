4724619894a95252513ce5e3c4b74577d68bd2a4
Dyego Soriano <dyegodf2@gmail.com>
2021-09-29 21:24:49 -0300

    feat: adding authenticate token

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,49 @@
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+
+import { IUserRepository } from '../../repositories/IUserRepository'
+
+interface IRequest {
+  password: string
+  email: string
+}
+
+interface IResponse {
+  user: {
+    name: string
+    email: string
+  }
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUserRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email)
+    if (!user) throw new Error('Email or password incorrect!')
+
+    const passwordMatch = await compare(password, user.password)
+    if (!passwordMatch) throw new Error('Email or password incorrect!')
+
+    const token = sign({}, 'ea4c8eec8d9652db33710edfccd5db37', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
+
+    return {
+      user: {
+        email: user.email,
+        name: user.name
+      },
+      token
+    }
+  }
+}
+
+export { AuthenticateUserUseCase }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 
+import { authenticateRoutes } from './authenticate.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRoutes } from './specifications.routes'
 import { usersRoutes } from './users.routes'
 
 const routes = Router()
 
-routes.use('/specifications', specificationsRoutes)
-routes.use('/categories', categoriesRoutes)
-routes.use('/users', usersRoutes)
+routes
+  .use('/specifications', specificationsRoutes)
+  .use('/categories', categoriesRoutes)
+  .use('/users', usersRoutes)
+  .use(authenticateRoutes)
 
 export { routes }
