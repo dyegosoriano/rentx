e9b5caf77fb6d9d7b72d6ebe9e773413a02a028b
Dyego Soriano <dyegodf2@gmail.com>
2021-10-03 15:04:07 -0300

    feat: adding error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata'
+import 'express-async-errors'
 import './shared/container'
 import './database'
 
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import swaggerUi from 'swagger-ui-express'
 
+import { AppError } from './erros/AppError'
 import { routes } from './routes'
 import swaggerFile from './swagger.json'
 
@@ -12,7 +14,18 @@ const port = process.env.PORT || 3333
 const app = express()
 
 app.use(express.json())
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ message: err.message })
+  }
+
+  console.log(err.stack)
+
+  return response.status(500).json({ status: 'error', message: 'Internal server error' })
+})
 
 app.listen(port, () => console.log(`🚀 Server is running port: ${port}`))
