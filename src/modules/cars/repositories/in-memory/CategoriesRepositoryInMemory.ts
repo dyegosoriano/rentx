552f9ff52b0cffbd717e111fa99ffde0a3f0e583
Dyego Soriano <dyegodf2@gmail.com>
2021-10-07 21:28:02 -0300

    feat: create first test

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,22 @@
+import { Category } from '../../entities/Category'
+import { ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository'
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = []
+
+  async create({ description, name }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category()
+    Object.assign(category, { description, name })
+    this.categories.push(category)
+  }
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find(item => item.name === name)
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories
+  }
+}
+
+export { CategoriesRepositoryInMemory }
