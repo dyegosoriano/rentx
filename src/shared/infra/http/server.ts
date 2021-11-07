8433dc8b4743e0e10c3f30ea0cdfca229f437c58
Dyego Soriano <dyegodf2@gmail.com>
2021-11-06 21:35:01 -0300

    feat: adding supertest

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,32 +1,5 @@
-import 'reflect-metadata'
-import 'express-async-errors'
-
-import express, { NextFunction, Request, Response } from 'express'
-import swaggerUi from 'swagger-ui-express'
-
-import { AppError } from '@shared/errors/AppError'
-import { routes } from '@shared/infra/http/routes'
-import createConnection from '@shared/infra/typeorm'
-import '@shared/container'
-
-import swaggerFile from '../../../swagger.json'
+import { app } from './app'
 
 const port = process.env.PORT || 3333
-const app = express()
-createConnection()
-
-app.use(express.json())
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use(routes)
-
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({ message: err.message })
-  }
-
-  console.log(err.stack)
-
-  return response.status(500).json({ status: 'error', message: 'Internal server error' })
-})
 
 app.listen(port, () => console.log(`🚀 Server is running port: ${port}`))
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,8 @@ export default async (host = 'database'): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host
+      database: process.env.NODE_ENV === 'test' ? 'rentx_test' : defaultOptions.database,
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host
     })
   )
 }
