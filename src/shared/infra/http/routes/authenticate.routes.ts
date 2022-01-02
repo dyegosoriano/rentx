2809254a6bd845cd588ccd7f65bf90f1f2346c3f
Dyego Soriano <dyegodf2@gmail.com>
2022-01-01 23:13:23 -0300

    feat: create RefreshTokenController

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 
 import { AuthenticateUserController } from '@modules/accounts/useCases/authenticateUser/AuthenticateUserController'
+import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/RefreshTokenController'
 
 const authenticateController = new AuthenticateUserController()
+const refreshTokenController = new RefreshTokenController()
 
 const authenticateRoutes = Router()
 
+authenticateRoutes.post('/refresh-token', refreshTokenController.handle)
 authenticateRoutes.post('/sessions', authenticateController.handle)
 
 export { authenticateRoutes }
