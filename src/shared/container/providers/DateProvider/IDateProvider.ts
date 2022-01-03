8342df0245e940e1beb1f7f160db07bc1d585074
Dyego Soriano <dyegodf2@gmail.com>
2022-01-03 16:52:39 -0300

    feat: adding hours on DateProvider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number
   compareInDays(start_date: Date, end_date: Date): number
   convertToUTC(date: Date): string
+  addHours(hours: number): Date
   addDays(days: number): Date
   dateNow(): Date
 }
