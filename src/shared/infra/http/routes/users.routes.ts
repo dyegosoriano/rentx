afa1c24d96fc6d4c39a254cb7af1fc189887fa29
Dyego Soriano <dyegodf2@gmail.com>
2022-01-16 17:20:31 -0300

    feat: ProfileUser useCase

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,18 +3,21 @@ import multer from 'multer'
 
 import uploadConfig from '@config/upload'
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController'
+import { ProfileUserController } from '@modules/accounts/useCases/profileUserUseCase/ProfileUserController'
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController'
 
 import { ensureAuthenticate } from '../middlewares/ensureAuthenticate'
 
 const updateUserAvatarController = new UpdateUserAvatarController()
 const createUserController = new CreateUserController()
+const profileUserController = new ProfileUserController()
 
 const usersRoutes = Router()
 const uploadAvatar = multer(uploadConfig)
 
 usersRoutes
   .patch('/', ensureAuthenticate, uploadAvatar.single('avatar'), updateUserAvatarController.handle)
+  .get('/profile', ensureAuthenticate, profileUserController.handle)
   .post('/', createUserController.handle)
 
 export { usersRoutes }
