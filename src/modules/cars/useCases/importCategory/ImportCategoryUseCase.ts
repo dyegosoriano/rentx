f61a9b2ac94cf31976dec38779a0e1ba55820196
Dyego Soriano <dyegodf2@gmail.com>
2022-06-16 17:31:36 -0300

    refactor: performando a importação de novas categorias

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -4,11 +4,6 @@ import { inject, injectable } from 'tsyringe'
 
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository'
 
-interface IImportCategory {
-  description: string
-  name: string
-}
-
 @injectable()
 class ImportCategoryUseCase {
   constructor(
@@ -16,38 +11,24 @@ class ImportCategoryUseCase {
     private CategoriesRepository: CategoriesRepository
   ) {}
 
-  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
-    return new Promise((resolve, reject) => {
-      const categories: IImportCategory[] = []
-
-      const stream = fs.createReadStream(file.path)
-      const parseFile = csvParse()
-
-      stream.pipe(parseFile)
-
-      parseFile
-        .on('data', line => {
-          const [name, description] = line
-          categories.push({ name, description })
-        })
-        .on('end', () => {
-          fs.promises.unlink(file.path)
-          resolve(categories)
-        })
-        .on('error', error => reject(error))
-    })
-  }
-
   async execute(file: Express.Multer.File): Promise<void> {
-    const categories = await this.loadCategories(file)
+    const stream = fs.createReadStream(file.path)
+    const parseFile = csvParse()
 
-    categories.map(async item => {
-      const { description, name } = item
+    stream.pipe(parseFile)
 
-      const existCategory = await this.CategoriesRepository.findByName(name)
+    parseFile
+      .on('data', async line => {
+        const [name, description] = line
 
-      if (!existCategory) await this.CategoriesRepository.create({ description, name })
-    })
+        const existCategory = await this.CategoriesRepository.findByName(name)
+        if (!existCategory) await this.CategoriesRepository.create({ description, name })
+      })
+      .on('end', () => fs.promises.unlink(file.path))
+      .on('error', error => {
+        fs.promises.unlink(file.path)
+        console.log(error)
+      })
   }
 }
 
